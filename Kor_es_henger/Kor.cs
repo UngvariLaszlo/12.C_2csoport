83d1a1af8e05d7124eb06a569507d888175a1d0a
UngvariLaci <ungvari.laszlo06@gmail.com>
2024-10-02 11:01:25 +0200

    korgenger elkezdve

---
+++ Kor_es_henger/Kor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kor_es_henger
+{
+    class Kor
+    {
+        //Az osztaly feladata egy kör területének és kerületének kiszámítása.
+
+        //Osztályváltozók deklarálása
+        private double sugar, kerület, terület;
+
+        //Konstruktorok
+        public Kor()
+        {
+
+        }
+
+        public Kor(double r)
+        {
+
+        } 
+    }
+}
