5602643cf05974a91b72b45a10287be37dbb1120
UngvariLaci <ungvari.laszlo06@gmail.com>
2024-11-13 10:59:14 +0100

    20241113

--- Kor_es_henger/Kor.cs
+++ Kor_es_henger/Kor.cs
@@ -3,50 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Kor_es_henger
+ 
+namespace KörHenger
 {
-    class Kor
+    class Kör
     {
-        //Az osztaly feladata egy kör területének és kerületének kiszámítása.
-
-        //Osztályváltozók deklarálása
-        private double sugar, kerület, terület;
-
-        //Konstruktorok
-        public Kor()
+        protected double sugar,
+            terület,
+            kerület;
+ 
+        public Kör() { }
+ 
+        public Kör(double r)
         {
-
+            this.sugar = r;
         }
-
-        public Kor(double r)
-        {
-            setDatas(r);
-        } 
-        private double kalkKerület(double r)
-        {
-            return Math.Round( 2 * r * Math.PI,2);
-        }
-
-        private double kalkTerület(double r)
-        {
-            return Math.Round(Math.Pow(r, 2) * Math.PI,2);
-        }
-
-        public double GetSugar() { return this.sugar; }
-        public double GetKerület() { return this.kerület; }
-        public double GetTerület() { return this.terület; }
+ 
         public void SetSugar(double r)
         {
             this.sugar = r;
-            this.kerület = kalkKerület(r);
-            this.terület = kalkTerület(r);
         }
-        private void setDatas(double r)
+ 
+        public void SetTerület()
         {
-            this.sugar = r;
-            this.kerület = kalkKerület(r);
-            this.terület = kalkTerület(r);
+            this.terület = Math.Round(Math.Pow(this.sugar, 2) * Math.PI, 2);
         }
+ 
+        public void SetKerület()
+        {
+            this.kerület = Math.Round(2 * this.sugar * Math.PI, 2);
+        }
+ 
+        public double GetTerület()
+        {
+            return this.terület;
+        }
+ 
+        public double GetKerület()
+        {
+            return this.kerület;
+        }
+ 
+        public double GetSugár()
+        {
+            return this.sugar;
+        }
+    }
+ 
+    class Henger : Kör
+    {
+        // Osztályváltozók
+        private double térfogat,
+            magasság;
+ 
+        // Konstruktor
+        public Henger(double s, double m)
+        {
+            // Változók beállítása
+            this.sugar = s;
+            this.magasság = m;
+ 
+            // Számítások elvégzése
+            SetKerület();
+            SetTerület();
+            this.térfogat = this.terület * this.magasság;
+        }
+ 
+        public double GetTérfogat()
+        {
+            return this.térfogat;
+        }
+ 
     }
 }
